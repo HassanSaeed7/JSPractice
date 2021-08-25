a9d61d3df6bbb4040071185c1ee5ef1f91383f10
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-08-25 02:11:02 -0700

    g

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function reverseString(str) {
       .reverse()
       .join("");
   }
-  console.log(reverseString("hello"));
+  reverseString("Let's Start");
   
   // CHALLENGE 2: VALIDATE A PALINDROME
   // Return true if palindrome and false if not
@@ -36,7 +36,7 @@ function reverseString(str) {
       return false;
     }
   }
-  console.log(palindrome("racecar"));
+  palindrome("racecar");
   
   //Only checks a word, not a sentence.
   function palindrome(str) {
@@ -47,7 +47,7 @@ function reverseString(str) {
       .join("");
     return str === palin;
   }
-  console.log(palindrome("Racecar"));
+  palindrome("Racecar");
   
   //This palindrome checker returns a word or sentence.
   function palindrome(str) {
@@ -64,7 +64,7 @@ function reverseString(str) {
         .join("")
     );
   }
-  console.log(palindrome("racecar"));
+  palindrome("racecar");
   
   // CHALLENGE 3: REVERSE AN INTEGER
   // Return an integer in reverse
@@ -78,7 +78,7 @@ function reverseString(str) {
       .join(""); //turns integer into string, split into array, reverse array, join aray.
     return parseInt(reverse1) * Math.sign(int); //returns string as integer and Math.sign() function returns the sign of the (int) that is passed into function
   }
-  console.log(reverseInt(-12345));
+  reverseInt(-12345);
   
   // CHALLENGE 4: CAPITALIZE LETTERS
   // Return a string with the first letter of every word capitalized
@@ -91,6 +91,8 @@ function reverseString(str) {
       .join(" ");
   }
   
+
+  
   // CHALLENGE 5: MAX CHARACTER
   // Return the character that is most common in a string
   // ex. maxCharacter('javascript') == 'a'
@@ -100,7 +102,7 @@ function reverseString(str) {
   // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
   function fizzBuzz() {}
   
-  // // Call Function
-  // const output = reverseString('hello');
+  // Call Function
+  const output = reverseString('hello');
   
-  // console.log(output);
+  console.log(output);
