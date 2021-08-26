f99b67c062a728252aca5a1dade4f009f69affd8
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-08-25 20:29:35 -0700

    g

--- script.js
+++ script.js
@@ -113,13 +113,94 @@ function repeatStringNumTimes(str, num) {
     repVar += str //this adds the str to the new variable while the loop condition remains true
     num--; //this increments the loop down to 0 so it exits the loop
   }
-  return repVar;
+  return repVar; //we return the new variable instead of the string passed into fx
 }
 repeatStringNumTimes("abc", 3);
 
+// CHALLENGE 7: TRUNCATE A STRING
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+function truncateString(str, num) {
+  if (str.length > num) { 
+    return str.slice(0, num) + "..."; //.slice() will slice out what you index in ().
+  } else {
+    return str;
+  }
+}
 
+// CHALLENGE 8: Confirm the Ending
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+// This challenge can be solved with the .endsWith() method
+// we would like you to use one of the JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("Bastian", "n");
+
+//str.length minus target.length gives us the index of the string we want to slice (the last number is indexed 1 less due to 0-indexing) 
+// then we compare that sliced to the target
+//Alternative: If a negative number is provided as the first parameter to slice() , the offset is taken backwards from the end of the string.
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target; 
+}
+
+confirmEnding("Bastian", "n");
+
+
+// CHALLENGE 9: Boo who
+//Check if a value is classified as a boolean primitive. Return true or false.
+//Boolean primitives are true and false.
+
+function booWho(bool) {
+  if (bool === true || bool === false) {
+  return true;
+} else {
+  return false;
+}
+}
+booWho(null);
   
-  // Call Function
+
+//  CHALLENGE 10: Slice and Splice
+// You are given two arrays and an index.
+//Copy each element of the first array into the second array, in order.
+//Begin inserting elements at index n of the second array.
+//Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  let container = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+   container.splice(n, 0, arr1[i]);
+   n++;
+}
+return container;
+}
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+// Code Explanation
+//Our goal is to take all of the elements from arr1 and insert them into arr2 starting with index position n. At the same time we must ensure that neither arr or arr2 have been mutated.
+
+// Using the slice() function we can create an exact replica of arr2 and assign the result of the operation to a variable, localArray.
+
+// Now that we have an array that we can mutate on, we can iterate through every item in the first array. For each item in the first array we can use the splice() function to insert the item into index n of localArray.
+
+// We increment the index n by one. This will ensure that every item from the arr1 is inserted into localArray in the proper index position.
+
+// Finally, we return the localArray and end the function.
+
+
+
+
+
+
+
+
+
+
+
+// Call Function
   const output = reverseString('hello');
   
   console.log(output);
