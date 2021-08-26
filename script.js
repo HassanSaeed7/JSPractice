0b232165b931bbd00c76cfe3af592c6984f3a77a
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-08-25 20:32:33 -0700

    g

--- script.js
+++ script.js
@@ -177,19 +177,22 @@ function frankenSplice(arr1, arr2, n) {
 }
 return container;
 }
-console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 // Code Explanation
 //Our goal is to take all of the elements from arr1 and insert them into arr2 starting with index position n. At the same time we must ensure that neither arr or arr2 have been mutated.
-
 // Using the slice() function we can create an exact replica of arr2 and assign the result of the operation to a variable, localArray.
-
 // Now that we have an array that we can mutate on, we can iterate through every item in the first array. For each item in the first array we can use the splice() function to insert the item into index n of localArray.
-
 // We increment the index n by one. This will ensure that every item from the arr1 is inserted into localArray in the proper index position.
-
 // Finally, we return the localArray and end the function.
 
+//If we want to use the spread operator to push the entire array all at once (instead of iterating through the array)
+function frankenSplice(arr1, arr2, n) {
+let container = arr2.slice();
+  container.splice(n, 0, ...arr1);
+  return container;
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 
 
