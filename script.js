3b545392401467b90ee1644ef25214e98951ac0f
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-08-26 17:39:53 -0700

    g

--- script.js
+++ script.js
@@ -229,6 +229,39 @@ function bouncer(arr) {
 bouncer([7, "ate", "", false, 9]);
 
 
+//Where do I Belong
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. 
+// The returned value should be a number.
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => (a - b)); //sort through the array 
+  for (let i = 0; i < arr.length; i++) { //loop through the array, comparing num to each iteration of arr
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+  return arr.length; //this ensures that you return an index position instead of the array
+}
+
+getIndexToIns([40, 60], 50);
+// First I sort the array using .sort(callbackFunction) to sort it by lowest to highest, from left to right.
+// Then I use a for loop to compare the items in the array starting from the smallest one. 
+// When an item on the array is greater than the number we are comparing against, then we return the index.
+
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num > val).length; //the .length ensures that you return an index position
+}
+// Count the number of entries that are smaller than the new value num
+// The new value would be inserted after these values
+
+
+
+
+
+
+
+
+
+
 
 // Call Function
   const output = reverseString('hello');
